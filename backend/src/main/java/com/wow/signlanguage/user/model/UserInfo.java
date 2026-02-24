b34384273bd2f36c38cde906b3e2185ebb913fb2
LakkeTree <parkjoyjoy@ajou.ac.kr>
2026-02-25 02:42:21 +0900

    Feat: 구글 로그인 시 firestore DB에 유저 정보 저장 - 지헌

---
+++ backend/src/main/java/com/wow/signlanguage/user/model/UserInfo.java
@@ -0,0 +1,75 @@
+package com.wow.signlanguage.user.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInfo {
+    private int correctQuestionNum;
+    private List<String> incorrectQuestions;
+    private int userId;
+    private int totalQuestionNum;
+    private int userLevel;
+    private List<String> totalQuestions;
+
+    public UserInfo() {
+        this.incorrectQuestions = new ArrayList<>();
+        this.totalQuestions = new ArrayList<>();
+    }
+
+    public UserInfo(int userId, int correctQuestionNum, int totalQuestionNum, int userLevel) {
+        this.userId = userId;
+        this.correctQuestionNum = correctQuestionNum;
+        this.totalQuestionNum = totalQuestionNum;
+        this.userLevel = userLevel;
+        this.incorrectQuestions = new ArrayList<>();
+        this.totalQuestions = new ArrayList<>();
+    }
+
+    public int getCorrectQuestionNum() {
+        return correctQuestionNum;
+    }
+
+    public void setCorrectQuestionNum(int correctQuestionNum) {
+        this.correctQuestionNum = correctQuestionNum;
+    }
+
+    public List<String> getIncorrectQuestions() {
+        return incorrectQuestions;
+    }
+
+    public void setIncorrectQuestions(List<String> incorrectQuestions) {
+        this.incorrectQuestions = incorrectQuestions;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getTotalQuestionNum() {
+        return totalQuestionNum;
+    }
+
+    public void setTotalQuestionNum(int totalQuestionNum) {
+        this.totalQuestionNum = totalQuestionNum;
+    }
+
+    public int getUserLevel() {
+        return userLevel;
+    }
+
+    public void setUserLevel(int userLevel) {
+        this.userLevel = userLevel;
+    }
+
+    public List<String> getTotalQuestions() {
+        return totalQuestions;
+    }
+
+    public void setTotalQuestions(List<String> totalQuestions) {
+        this.totalQuestions = totalQuestions;
+    }
+}
